5759e771a54e936cca7156e0317d4be5ad74c463
Jae-eun <je4297@naver.com>
2019-01-21 00:30:54 +0900

    모델 분리 다시 커밋

--- BoxOffice/Model/CommentModel.swift
+++ BoxOffice/Model/CommentModel.swift
@@ -7,3 +7,27 @@
 //
 
 import Foundation
+
+/// 댓글 응답 객체
+struct CommentsList: Codable {
+    let movieId: String
+    let comments: [Comment]
+    
+    enum CodingKeys: String, CodingKey {
+        case comments
+        case movieId = "movie_id"
+    }
+}
+
+struct Comment: Codable {
+    let rating: Double
+    let timestamp: Double
+    let writer: String
+    let movieId: String
+    let contents: String
+    
+    enum CodingKeys: String, CodingKey {
+        case rating, timestamp, writer, contents
+        case movieId = "movie_id"
+    }
+}
--- BoxOffice/Model/MovieModel.swift
+++ BoxOffice/Model/MovieModel.swift
@@ -7,3 +7,32 @@
 //
 
 import Foundation
+
+/// 영화 리스트 응답 객체
+struct APIResponse: Codable {
+    let orderType: Int
+    let movies: [Movie]
+    
+    enum CodingKeys: String, CodingKey {
+        case movies
+        case orderType = "order_type"
+    }
+}
+
+struct Movie: Codable {
+    let title: String
+    let date: String
+    let thumb: String
+    let reservationGrade: Int
+    let userRating: Double
+    let grade: Int
+    let reservationRate: Double
+    let id: String
+    
+    enum CodingKeys: String, CodingKey {
+        case title, date, thumb, grade, id
+        case reservationGrade = "reservation_grade"
+        case userRating = "user_rating"
+        case reservationRate = "reservation_rate"
+    }
+}
